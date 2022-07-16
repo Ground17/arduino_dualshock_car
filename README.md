f3d5a12ee4738c7cf6a6c504098ec0dd6641cfcb
jisang-dev <hyla981020@gmail.com>
2022-07-16 14:44:41 +0900

    added real photo and completed ino code

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
 # arduino dualshock for RC car
 
-## blueprint
-![alt text](./blueprint.png)
+## Blueprint
+![blueprint](./blueprint.png)
+It's a very brief blueprint. Switch, resistance, usb dongle, etc. were omitted, and usb host shield and motor driver were smaller than the design drawing. The pin number may also be different from the actual one.
+
+`PIN9 == INT, PIN10 == SS, PIN11 == MOSI, PIN12 == MISO, PIN13 == SCK`
+
+## Photo
+![photo](./photo.jpg)
+
+## Problems and Solutions
+1. When usb is not running well
+    - Verify that the RST of usb is connected to the VCC together. [Reference](https://ht-deko.com/arduino/shield_usbhost_mini.html)
+    - You didn't have to cut the 5 volt cable
+    - Make sure that the pins are connected correctly. Depending on the USB host shield product, MISO, MOSI, and CLK may be printed incorrectly.
+2. The motor driver works well when two motors are operated, but only one is not operated
+    - Set the motor voltage on the off side to HIGH-HIGH instead of LOW-LOW
+3. When the Bluetooth connection disconnects often,
+    - Presumably due to too low current flow in usb
+    - Provide appropriate current to usb
+
 
 ## Reference
 https://maker.pro/arduino/projects/how-to-control-an-arduino-robot-with-a-ps4-bluetooth-controller  
-https://github.com/felis/USB_Host_Shield_2.0/blob/master/examples/Bluetooth/PS4BT/PS4BT.ino
+https://github.com/felis/USB_Host_Shield_2.0/blob/master/examples/Bluetooth/PS4BT/PS4BT.ino  
+https://comocloud.tistory.com/207#recentComments  
+https://ht-deko.com/arduino/shield_usbhost_mini.html
--- arduino_dualshock.ino
+++ arduino_dualshock.ino
@@ -1,4 +1,5 @@
 #include <PS4BT.h>
+/// #include <PS4USB.h>
 #include <usbhub.h>
 
 // Satisfy the IDE, which needs to see the include statement in the ino too.
@@ -6,13 +7,15 @@
 #include <spi4teensy3.h>
 #endif
 #include <SPI.h>
-int IN1 = 3;       //control pin for first motor
-int IN2 = 4;       //control pin for first motor
-int IN3 = 5;        //control pin for second motor
-int IN4 = 6;        //control pin for second motor
 
-// PIN9 == INT, PIN10 ==SS, PIN11 == MOSI, PIN12 == MISO, PIN13 == SCK
+const int IN1 = 2;       //control pin for first motor
+const int IN2 = 3;       //control pin for first motor
+const int IN3 = 4;       //control pin for second motor
+const int IN4 = 5;       //control pin for second motor
+
+// PIN9 == INT, PIN10 == SS, PIN11 == MOSI, PIN12 == MISO, PIN13 == SCK
 USB Usb;
+/// PS4USB PS4(&Usb);
 //USBHub Hub1(&Usb); // Some dongles have a hub inside
 BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
 
@@ -24,15 +27,13 @@ PS4BT PS4(&Btd, PAIR);
 // After that you can simply create the instance like so and then press the PS button on the device
 //PS4BT PS4(&Btd);
 
-bool printAngle, printTouch;
-uint8_t oldL2Value, oldR2Value;
-
 void setup() {
+  Serial.begin(115200);
   pinMode(IN1, OUTPUT);  
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);  
   pinMode(IN4, OUTPUT);
-  Serial.begin(115200);
+  
 #if !defined(__MIPSEL__)
   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
 #endif
@@ -46,36 +47,81 @@ void setup() {
 
 void loop() {
   Usb.Task();
-
   if (PS4.connected()) {
-    if (PS4.getButtonClick(TRIANGLE)) {
-      digitalWrite(IN1, HIGH);
-      digitalWrite(IN2, LOW);
-      Serial.print(F("\r\nTriangle"));
-      // PS4.setRumbleOn(RumbleLow);
-    } else if (PS4.getButtonClick(CROSS)) {
+/* check the signal */
+//    Serial.print(F("\r\nLeftHatX: "));
+//    Serial.print(PS4.getAnalogHat(LeftHatX));
+//    Serial.print(F("\tLeftHatY: "));
+//    Serial.print(PS4.getAnalogHat(LeftHatY));
+//    Serial.print(F("\tRightHatX: "));
+//    Serial.print(PS4.getAnalogHat(RightHatX));
+//    Serial.print(F("\tRightHatY: "));
+//    Serial.print(PS4.getAnalogHat(RightHatY));
+//    Serial.print("\n");
+
+/* It was written to simplify the code, but for some reason, there are a lot of errors */
+//    int rightYValue = PS4.getAnalogHat(RightHatY);
+//    int leftXValue = PS4.getAnalogHat(LeftHatX);
+
+//    activeMotor(
+//      rightYValue < 20, 
+//      rightYValue > 236, 
+//      leftXValue < 20, 
+//      leftXValue > 236
+//    );
+
+    if (20 <= PS4.getAnalogHat(RightHatY) && PS4.getAnalogHat(RightHatY) <= 236 
+      && 20 <= PS4.getAnalogHat(LeftHatX) && PS4.getAnalogHat(LeftHatX) <= 236 ) {
       digitalWrite(IN1, LOW);
-      digitalWrite(IN2, HIGH);
-      Serial.print(F("\r\nCross"));
-      // PS4.setRumbleOn(RumbleLow);
-    } else {
-      digitalWrite(IN1, LOW);
       digitalWrite(IN2, LOW);
-      Serial.print(F("\r\nclick 'O' or 'X'"));
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      return;
     }
     
-    if(PS4.getAnalogHat(LeftHatX) < 20) {
+    if (PS4.getAnalogHat(RightHatY) < 20) { // up
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+    } else if (PS4.getAnalogHat(RightHatY) > 236) { // down
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+    } else {
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, HIGH);
+      Serial.print("\nrotate the right joystick to move forward/backward");
+    }
+    
+    if (PS4.getAnalogHat(LeftHatX) < 20) { // left
       digitalWrite(IN3, LOW);
       digitalWrite(IN4, HIGH);
-      Serial.print("\nHatX: 50 ");
-    } else if(PS4.getAnalogHat(LeftHatX) > 236) {
+    } else if (PS4.getAnalogHat(LeftHatX) > 236) { // right
       digitalWrite(IN3, HIGH);
       digitalWrite(IN4, LOW);
-      Serial.print("\nHatX: 200 ");
     } else {
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, HIGH);
+      Serial.print("\nrotate the joystick to move left/right");
+    }
+  } else {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
       digitalWrite(IN3, LOW);
       digitalWrite(IN4, LOW);
-      Serial.print("\nrotate the joystick");
-    }
   }
 }
+
+//void activeMotor(bool isIn1, bool isIn2, bool isIn3, bool isIn4) {
+//  digitalWrite(IN1, isIn1 ? HIGH : LOW);
+//  digitalWrite(IN2, isIn2 ? HIGH : LOW);
+//  digitalWrite(IN3, isIn3 ? HIGH : LOW);
+//  digitalWrite(IN4, isIn4 ? HIGH : LOW);
+//  
+//  Serial.print(isIn1 ? HIGH : LOW);
+//  Serial.print(" ");
+//  Serial.print(isIn2 ? HIGH : LOW);
+//  Serial.print(" ");
+//  Serial.print(isIn3 ? HIGH : LOW);
+//  Serial.print(" ");
+//  Serial.print(isIn4 ? HIGH : LOW);
+//  Serial.print("\n");
+//}
